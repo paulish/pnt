cce7d41ee56282895f5bb686927e4c830c837a61
Paul Ishenin <paul.ishenin@gmail.com>
2017-09-11 10:22:35 +0700

    * поправил старт

--- app.js
+++ app.js
@@ -36,8 +36,4 @@ app.use(function(err, req, res, next) {
     console.log(err);
 });
 
-app.set('port', 5000);
-
-var server = app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + server.address().port);
-});
+module.exports = app;
