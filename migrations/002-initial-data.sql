938a8da18e75b54feddabe495fb4dabd77c17566
Paul Ishenin <paul.ishenin@gmail.com>
2018-01-08 00:20:26 +0700

    update db

--- migrations/002-initial-data.sql
+++ migrations/002-initial-data.sql
@@ -1,20 +1,24 @@
 -- Up 
 
-INSERT INTO PLAYER (name, tag) VALUES ('paulish', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('A_G_R_O_N_O_M', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('SherXan', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('CkuJIJIa_HET', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('Kira-5let', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('K_H_9l_3_b-T_b_M_bl', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('Whisky', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('npocmume', 'PNT');
-INSERT INTO PLAYER (name, tag) VALUES ('kratasuk85', 'PNT2');
-INSERT INTO PLAYER (name, tag) VALUES ('6eru_OT-MeH9l', 'PNT2');
-INSERT INTO PLAYER (name, tag) VALUES ('evgen150rus', 'PNT2');
+INSERT INTO PLAYER (name, tag, idx) VALUES ('paulish', 'PNT', 1);
+INSERT INTO PLAYER (name, tag, idx) VALUES ('A_G_R_O_N_O_M', 'PNT', 2);
+INSERT INTO PLAYER (name, tag, idx) VALUES ('Kira-5let', 'PNT', 3);
+INSERT INTO PLAYER (name, tag, idx) VALUES ('K_H_9l_3_b-T_b_M_bl', 'PNT', 4);
+INSERT INTO PLAYER (name, tag, idx) VALUES ('Whisky', 'PNT', 5);
+INSERT INTO PLAYER (name, tag, idx) VALUES ('npocmume', 'PNT', 6);
+INSERT INTO PLAYER (name, tag, idx) VALUES ('kratasuk85', 'PNT', 7);
+
+INSERT INTO PLAYER (name, tag) VALUES ('KaBka3ckuu_TIJIeHHuk', 'nHm');
+INSERT INTO PLAYER (name, tag) VALUES ('JART_7', 'nHm');
+INSERT INTO PLAYER (name, tag) VALUES ('evgen150rus', 'nHm');
+INSERT INTO PLAYER (name, tag) VALUES ('Pikassic', 'nHm');
+INSERT INTO PLAYER (name, tag) VALUES ('6eru_OT-MeH9l', 'nHm');
+INSERT INTO PLAYER (name, tag) VALUES ('xxx_LLI_u_x_a_H_xxx', 'nHm');
+
 INSERT INTO PLAYER (name, tag) VALUES ('egor55555_tankist', 'PNT2');
-INSERT INTO PLAYER (name, tag) VALUES ('xxx_LLI_u_x_a_H_xxx', 'PNT2');
-INSERT INTO PLAYER (name, tag) VALUES ('JART_7', 'PNT2');
-INSERT INTO PLAYER (name, tag) VALUES ('KaBka3ckuu_TIJIeHHuk', 'PNT2');
+
+INSERT INTO PLAYER (name, tag) VALUES ('SherXan', null);
+INSERT INTO PLAYER (name, tag) VALUES ('CkuJIJIa_HET', null);
 INSERT INTO PLAYER (name, tag) VALUES ('KyKoJIkA', null);
 INSERT INTO PLAYER (name, tag) VALUES ('D_O_H_6_A_C_C', null);
 INSERT INTO PLAYER (name, tag) VALUES ('olga7410', null);
@@ -25,13 +29,15 @@ INSERT INTO PLAYER (name, tag) VALUES ('SWALLOW_THE_SUN', null);
 INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-B', 1, 'firebird', 'twins', 'ricochet', 1);
 INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Барсук T-F', 1, 'firebird', 'freeze', 'twins', 2);
 INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Барсук T-D', 1, 'firebird', 'freeze', 'thunder', 3);
-INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-C', 1, 'freeze', 'shotgun', 'ricochet', NULL);
-INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-A', 1, 'firebird', 'shotgun', 'twins', NULL);
-INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Акула T-G', 2, 'ricochet', 'rocket_launcher', 'thunder', NULL);
-INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-D', 2, 'shotgun', 'twins', 'ricochet', NULL);
-INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Кодиак T-K', 2, 'freeze', 'rocket_launcher', 'artillery', NULL);
-INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-F', 3, 'rocket_launcher', 'twins', 'ricochet', NULL);
-INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Урса T-J', 3, 'twins', 'rocket_launcher', 'artillery', NULL);
+INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-C', 1, 'freeze', 'shotgun', 'ricochet', 4);
+
+INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-A', 2, 'firebird', 'shotgun', 'twins', 1);
+INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-D', 2, 'shotgun', 'twins', 'ricochet', 2);
+INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Акула T-G', 2, 'ricochet', 'rocket_launcher', 'thunder', 3);
+INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Кодиак T-K', 2, 'freeze', 'rocket_launcher', 'artillery', 4);
+INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Лев T-F', 2, 'rocket_launcher', 'twins', 'ricochet', 5);
+INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Урса T-J', 2, 'twins', 'rocket_launcher', 'artillery', 6);
+
 INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Гризли T-L', 3, 'freeze', 'shotgun', 'rocket_launcher', NULL);
 INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Барсук T-J', 3, 'firebird', 'isis', 'rocket_launcher', NULL);
 INSERT INTO module (name, "rank", res1, res2, res3, idx) VALUES('Гризли T-H', 3, 'freeze', 'ricochet', 'machine_gun', NULL);
