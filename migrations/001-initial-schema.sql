1a8812119aede89ad85750dfd7ced5adba857e89
Paul Ishenin <paul.ishenin@gmail.com>
2018-01-09 19:54:44 +0700

    * work on discord bot

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -1,5 +1,5 @@
 -- Up 
-CREATE TABLE IF NOT EXISTS player(id integer not null primary key, name varchar(100) not null, tag varchar(10), idx integer, discord_id varchar(100));
+CREATE TABLE IF NOT EXISTS player(id integer not null primary key, name varchar(100) not null, tag varchar(10), idx integer, discord_id varchar(100), udate datetime);
 CREATE TABLE IF NOT EXISTS module(id integer not null primary key, name varchar(20) not null, rank integer not null, res1 varchar2(20), res2 varchar2(20), res3 varchar2(20), idx integer);
 CREATE TABLE IF NOT EXISTS hull(id integer not null primary key, name varchar(20) not null, rank integer not null, idx integer);
 CREATE TABLE IF NOT EXISTS turret(id integer not null primary key, name varchar(20) not null, eng_name varchar(20), rank integer not null, idx integer);
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS player_modules(
   time_played integer not null, 
   mu integer,
   crdate datetime default CURRENT_TIMESTAMP,
+  PRIMARY KEY (idplayer, idmodule)
   FOREIGN KEY(idplayer) REFERENCES player(id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(idmodule) REFERENCES module(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -18,6 +19,7 @@ CREATE TABLE IF NOT EXISTS player_hulls(
   time_played integer not null, 
   mu integer,
   crdate datetime default CURRENT_TIMESTAMP,
+  PRIMARY KEY (idplayer, idhull),
   FOREIGN KEY(idplayer) REFERENCES player(id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(idhull) REFERENCES hull(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -27,6 +29,7 @@ CREATE TABLE IF NOT EXISTS player_turrets(
   time_played integer not null, 
   mu integer,
   crdate datetime default CURRENT_TIMESTAMP,
+  PRIMARY KEY (idplayer, idturret),
   FOREIGN KEY(idplayer) REFERENCES player(id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY(idturret) REFERENCES turret(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
